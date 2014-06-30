20d3f7a68d3eead4eefc7903b6b740a41b980410
Paul Covington <pcovington@jawbone.com>
2014-06-30 11:12:43 -0700

    latest

--- js/pool.js
+++ js/pool.js
@@ -1,21 +1,31 @@
+var socket = io.connect('http://pcovi-mbr-11046:8000');
 
+var factor = 0.5;
 var rolling_mag;
 function deviceMotionHandler(eventData) {
     accel = eventData.acceleration;
     mag = Math.sqrt(accel.x*accel.x +
 		    accel.y*accel.y +
 		    accel.z*accel.z);
-    factor = 0.7;
     if (rolling_mag == null) {rolling_mag = mag;}
+
     rolling_mag = factor*mag + (1.0-factor)*rolling_mag;
 
-    if (Math.abs(mag-rolling_mag) > 1.5) {
-	document.getElementById("main").style.backgroundColor = "green";
+    if ( (mag-rolling_mag) > 3.0) {
+	socket.emit('bump', new Date().getTime())
+	//document.getElementById("main").style.backgroundColor = "green";
     }
     else {
-	document.getElementById("main").style.backgroundColor = "red";
+	//document.getElementById("main").style.backgroundColor = "red";
     }
 }
+
+socket.on('green', function() {
+  document.getElementById("main").style.backgroundColor = "green";
+  setTimeout(function() {
+        document.getElementById("main").style.backgroundColor = "red";
+  }, 1000);
+});
 
 if (window.DeviceMotionEvent) {
     console.log("DeviceMotionEvent supported");
